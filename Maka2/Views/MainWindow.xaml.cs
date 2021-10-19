88b6701e890be8e3669918bf2aa8ed058106d5b5
KevynESP <kevinnivekferreira@gmail.com>
2021-10-19 16:59:20 +0200

    2º Commit inicio conexion

---
+++ Maka2/Views/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Maka2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket socket;
+        EndPoint epLocal, epRemote;
+        byte[] buffer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            TMensaje.Text = "Write down...";
+
+            IpLocal.Content = GetLocalIp();
+            IpRemote.Content = IPPublica();
+        }
+
+        private string GetLocalIp()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "127.0.0.1";
+        }
+
+        private string IPPublica()
+        {
+            string externalip = new WebClient().DownloadString("http://icanhazip.com");
+            return externalip;
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void ButtonMaximaze_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void ConnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //bind socket
+                epLocal = new IPEndPoint(IPAddress.Parse(LocalIp.Text.Trim()), Convert.ToInt32(LocalPort.Text.Trim()));
+                socket.Bind(epLocal);
+                //Connect to remote
+                epRemote = new IPEndPoint(IPAddress.Parse(RemoteIp.Text.Trim()), Convert.ToInt32(RemotePort.Text.Trim()));
+                socket.Connect(epRemote);
+                //List the port
+                buffer = new byte[1500];
+                socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                byte[] receivedData = new byte[1500];
+                receivedData = (byte[])aResult.AsyncState;
+                //Convert to str
+                ASCIIEncoding aEncoding = new ASCIIEncoding();
+                string receivedMessage = aEncoding.GetString(receivedData);
+
+                //add Messages
+                lMensajes.Items.Add(DateTime.Now.ToShortTimeString() + " Friend: " + receivedMessage);
+
+                buffer = new byte[1500];
+                socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void TMensaje_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TMensaje.Text = "";
+        }
+
+        private void EnviarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Convert string byte
+            ASCIIEncoding aEncoding = new ASCIIEncoding();
+            byte[] sendingMessage = new byte[1500];
+            sendingMessage = aEncoding.GetBytes(TMensaje.Text);
+            //Send 
+            socket.Send(sendingMessage);
+            //add message
+            lMensajes.Items.Add(DateTime.Now.ToShortTimeString() + " Me: " + TMensaje.Text);
+            TMensaje.Text = "";
+        }
+
+    }
+}
